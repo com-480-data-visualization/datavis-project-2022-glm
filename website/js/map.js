9d6578b6b2a45034bbb556414df299a1d10061bb
Mihaela Berezantev <berezantev_mihaela@mail.ru>
2022-05-31 17:46:13 +0200

    add tooltip

--- website/js/map.js
+++ website/js/map.js
@@ -1,13 +1,6 @@
 class MapPlot {
 
-	constructor(svg_element_id) {
-		this.svg = d3.select('#' + svg_element_id);
-
-		// may be useful for calculating scales
-		const svg_viewbox = this.svg.node().viewBox.animVal;
-		this.svg_width = svg_viewbox.width;
-		this.svg_height = svg_viewbox.height;
-
+	constructor(div_element_id) {
 
 		const map_promise = d3.json("./data/countries.json").then((topojson_raw) => {
 			const countries_paths = topojson.feature(topojson_raw, topojson_raw.objects.countries);
@@ -15,8 +8,6 @@ class MapPlot {
 		});
 
 		const data_promise = d3.csv("./data/World_Energy_Consumption.csv").then((data) => {
-			// process the energy data here
-			//console.log(data)
 			return data;
 		});
 
@@ -48,23 +39,29 @@ class MapPlot {
 
 
 			//****** Heatmap legend ******
+			const width = 1200
+			const height = 600
+			var svg = d3.select("#" + div_element_id)
+			.append("svg")
+			.attr("width", width)
+			.attr("height", height)
 
 			//The <defs> element is used to store graphical objects that will be used at a later time.
-			const defs = this.svg.append("defs");
+			const defs = svg.append("defs");
 			const linearGradient = defs.append("linearGradient")
 			.attr("id", "linear-gradient");
 
 			const greens = [d3.color("#B7E4C7"), d3.color("#95D5B2"), d3.color("#74C69D"), d3.color("#52B788"),
 			 								d3.color("#40916C"), d3.color("#2D6A4F"), d3.color("#1B4332")]
 			const colorScale = d3.scaleSequential().interpolator(d3.interpolateRgbBasis(greens)).domain(value_range)
-			const margin = ({top: 20, right: 150, bottom: 30, left: 150})
-			const barHeight = 20
 
-			this.svg.append('g')
-			.attr("transform", `translate(0,${this.svg_height - margin.bottom - barHeight})`)
+			const barHeight = 20
+			const margin = ({top: 20, right: 150, bottom: 30, left: 150})
+			svg.append('g')
+			.attr("transform", `translate(0,${height - margin.bottom - barHeight})`)
 			.append("rect")
 			.attr('transform', `translate(${margin.left}, 0)`)
-			.attr("width", this.svg_width - margin.right - margin.left)
+			.attr("width", width - margin.right - margin.left)
 			.attr("height", barHeight)
 			.style("fill", "url(#linear-gradient)");
 
@@ -76,48 +73,77 @@ class MapPlot {
 
 			let axisScale = d3.scaleLinear()
 			.domain(colorScale.domain())
-			.range([margin.left, this.svg_width - margin.right])
+			.range([margin.left, width - margin.right])
 
 			let axisBottom = g => g
 			.attr("class", `x-axis`)
-			.attr("transform", `translate(0,${this.svg_height - margin.bottom})`)
+			.attr("transform", `translate(0,${height - margin.bottom})`)
 			.call(d3.axisBottom(axisScale)
-			.ticks(this.svg_width / 100)
+			.ticks(width/ 100)
 			.tickSize(-barHeight))
 
-			this.svg.append('g')
+			svg.append('g')
 			.call(axisBottom);
 
-
 			//****** Draw countries******
+
 			var projection = d3.geoNaturalEarth1()
 			.center([0, 0]) //geographical center
 			.scale(175)
-			.translate([this.svg_width/2, this.svg_height/2]) //center in SVG coordinate space
-
-			//map_data.features = map_data.features.filter(function(d){console.log(d.properties.name) ; return d.properties.name=="France"})
-
-			//map_data.map(x => console.log(x))
+			.translate([width/2, height/2]) //center in SVG coordinate space
 
 			const path = d3.geoPath(projection);
 
-			const in_map_not_in_energy = map_data.filter(value => ! energy_data_2005.map(x => x.country).includes(value.properties.name));
-			const in_energy_not_in_map = energy_data_2005.filter(value => ! map_data.map(x => x.properties.name).includes(value.country))
-			//in_energy_not_in_map.map(x => console.log(x.country))
+			// create a tooltip to display country name
+	  	var tooltip = d3.select("#" + div_element_id)
+				.append("div")
+				.style("position", "absolute")
+		    .style("visibility", "hidden")
+				.style("background-color", "white")
+		    .style("border", "solid")
+		    .style("border-width", "2px")
+		    .style("border-radius", "5px")
+		    .style("padding", "5px")
 
-			//in_map_not_in_energy.map(x => console.log(x.properties.name))
 
-			this.svg.selectAll("path")
+			// Three functions that change the tooltip when user hover / move / leave a cell
+			var mouseover = function(d) {
+				tooltip
+		      .style("visibility", "visible")
+		    d3.select(this)
+		      .style("stroke", "black")
+		      .style("opacity", 1)
+			}
+
+			var mousemove = function(d) {
+				var html = 0
+		    if (d.renew_cons === "unavailable data") html = tooltip.html(d.properties.name + "<br>" + "unavailable data")
+				else html = tooltip.html(d.properties.name + "<br>" + d.renew_cons + " TWh")
+
+				return html.style("top", (event.pageY - 20)+"px").style("left",(event.pageX + 20)+"px")
+		  }
+
+			var mouseleave = function(d) {
+				tooltip
+					.style("visibility", "hidden")
+				d3.select(this)
+					.style("stroke", "none")
+			}
+
+			svg.selectAll("path")
 			.data(map_data)
 			.enter().append("path")
 			// draw each country
 			.attr("d", path)
-
 			// set the color of each country
 			.attr("fill", function (d) {
-        d.renew_cons = map_country_renew_cons.get(d.properties.name) || "unavailable_data";
-				if(d.renew_cons === "unavailable_data") return "#ADB5BD"
-        return colorScale(d.renew_cons);});
+        d.renew_cons = map_country_renew_cons.get(d.properties.name) || "unavailable data";
+				if(d.renew_cons === "unavailable data") return "#ADB5BD"
+        return colorScale(d.renew_cons);})
+			.on("mouseover", mouseover)
+	    .on("mousemove", mousemove)
+	    .on("mouseleave", mouseleave)
+
 		});
 	}
 }
