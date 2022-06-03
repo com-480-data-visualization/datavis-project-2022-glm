3e20245eb5ed85882128219260d8e9292488ada4
Gil Tinde <giltinde@hotmail.com>
2022-06-03 13:13:13 +0200

    updated area chart

--- website/js/stacked_basic_area_chart.js
+++ website/js/stacked_basic_area_chart.js
@@ -2,12 +2,11 @@ class StackedBasicAreaChart {
 
 	constructor(element_id) {
 
+		// Load data and keep only year, country, gas, coal, oil, solar, wind, hydro
 		d3.csv("./data/World_Energy_Consumption.csv",
 		d => {
 			return {
 				year: d3.timeParse("%Y")(d.year),
-				// year: parseInt(d.year),
-				// values: [d.gas_consumption, d.coal_consumption, d.oil_consumption,d.solar_consumption,d.wind_consumption,d.hydro_consumption],
 				gas: d.gas_consumption,
 				coal: d.coal_consumption,
 				oil: d.oil_consumption,
@@ -18,71 +17,51 @@ class StackedBasicAreaChart {
 			}
 		}).then((data) => {
 
+			////////////////////
+			// DATA AND MAIN SVG
+
+			// Data loaded successfully
 			var energy_data = data;
-
-
 			console.log('Data loaded')
+
+			// Filter data to use "World" country and years between 1965 and 2019
 			const post_1965 = new Date('January 1, 1964 00:00:01');
 			const pre_2020 = new Date('January 1, 2020 00:00:00');
-
 			const to_include = ['World']
-			// energy_data = energy_data.filter(x => x.value !=0 && to_include.includes(x.country))
-			// energy_data = energy_data.filter(x => !x.values.includes("") && to_include.includes(x.country))
 			energy_data = energy_data.filter(x => 
 				x.year.getTime() < pre_2020.getTime() &&
 				x.year.getTime() > post_1965.getTime() && 
 				to_include.includes(x.country))
 
-
-			// energy_data = energy_data.filter(x => 
-			// 	x.year < 2020 &&
-			// 	x.year > 1964 && 
-			// 	to_include.includes(x.country))
-
-
-			// // group the data: one array for each value of the X axis.
-			// const grouped_on_year = d3.group(energy_data, d => d.year);
-
-
-
-			// Stack the data: each group will be represented on top of each other
-			// const energy_types = ["Gas", "Coal", "Oil", "Solar", "Wind", "Hydro"]
-			// const energy_idx = Array(energy_types.length).fill().map((_, i) => i)
-			// const stackedData = d3.stack()
-			// 	.keys(energy_idx)
-			// 	.value(function(d, key){
-			// 		return d.values[key]})
-			// 	(energy_data)
-
-			const energy_keys = ["gas", "coal", "oil","solar","wind","hydro"]
+			// Prepare constants for stacked data
+			const energy_keys = ["none","gas", "coal", "oil", "solar", "wind", "hydro"]
+			const color_mappings = ['#FFFFFF','#7B6D8D','#B3A394','#595457','#FFD166','#226F54','#73C1C6']
+			const offset = d3.stackOffsetExpand
 			const order = d3.stackOrderDescending
+
+			// Create stacked with full range of values
 			var stackGen = d3.stack()
 				.keys(energy_keys)
 				.order(order);
-
 			var stackedData = stackGen(energy_data)
-			// stackedData = stackedData.map(d => {
-			// 	d.forEach(v => {
-			// 	  v.key = d.key;
-			// 	  v.data.name = v.data[groupBy];
-			// 	});
-			  
-			// 	return d;
-			//   })
-			// console.log(stackedData[0].key)
-			// console.log(stackedData[0][0].data)
-			// console.log(stackedData)
-			// console.log(energy_data)
+	
+			// Create stacked with expanded range of values (between 0 and 1)
+			var stackGen_expanded = d3.stack()
+				.keys(energy_keys)
+				.order(order)
+				.offset(offset);
+			var stackedDataExpanded = stackGen_expanded(energy_data)
 
-			const margin = {top: 10, right: 30, bottom: 30, left: 50},
-			width = 760 - margin.left - margin.right,
-			height = 400 - margin.top - margin.bottom;
+			// Create main svg with dimensions
+			const margin = {top: 40, right: 30, bottom: 30, left: 50},
+			width = 860 - margin.left - margin.right,
+			height = 500 - margin.top - margin.bottom;
 
 			const svg = d3.select("#"+element_id)
 				.append("svg")
 					.attr("width", width + margin.left + margin.right)
 					.attr("height", height + margin.top + margin.bottom)
-				.append("g")
+			var inner = svg.append("g")
 					.attr("transform",
 						  "translate(" + margin.left + "," + margin.top + ")");
 
@@ -92,7 +71,7 @@ class StackedBasicAreaChart {
 						.range([ 0, width]);
 
 			// Add X axis, rotate tick labels to fit more date ticks
-			svg.append("g")
+			var X_AXIS = inner.append("g")
 			   .attr("transform", "translate(0,"+height+")")
 			   .call(d3.axisBottom(x_axis).ticks(d3.timeYear.every(3))) // tick every 2 years
 			   .selectAll("text")  
@@ -101,67 +80,149 @@ class StackedBasicAreaChart {
 			   .attr("dy", ".5em")
 			   .attr("transform", "rotate(-35)"); // rotate by 35 degrees
 
-			// Create Y axis
-			const y_axis = d3.scaleLinear()
-				.domain(
-					[0, d3.max( // take max over all entries (years) of sum of all energies
-						energy_data, d => 
-							parseInt(d.gas) + 
-							parseInt(d.coal) + 
-							parseInt(d.oil) +
-							parseInt(d.solar) + 
-							parseInt(d.wind) + 
-							parseInt(d.hydro))])
+			// Compute max y value tick for full range
+			// Take max over all entries (years) of sum of all energies
+			const y_max = d3.max(
+								energy_data, d => 
+									parseInt(d.gas) + 
+									parseInt(d.coal) + 
+									parseInt(d.oil) +
+									parseInt(d.solar) + 
+									parseInt(d.wind) + 
+									parseInt(d.hydro)
+								)*1.1
+
+			// Create Y axis (default full range)
+			var y_axis = d3.scaleLinear()
+				.domain([0, y_max])
 				.range([ height, 0 ]);
 
 			// Add Y axis
-			svg.append("g")
+			var Y_AXIS = inner.append("g")
 				.call(d3.axisLeft(y_axis));
 
-			// color palette
+			// Define color palette
 			const colorScale = d3.scaleOrdinal()
 				.domain(energy_keys)
-				.range(['#e41a1c','#377eb8','#4daf4a','#984ea3','#ff7f00','#ffff33'])
-		  
-			var areaGen = d3.area()
-				.x((d) => x_axis(d.data.year))
-				.y0((d) => y_axis(d[0]))
-				.y1((d) => y_axis(d[1]));
+				.range(color_mappings)
 
-			svg.selectAll("my-area")
-				.data(stackedData)
-				.enter()
-				.append("path")
-				.style("fill", (d) => colorScale(d.key))
-				.attr("d", areaGen)
+			// Add X axis label:
+			var X_AXIS_LABEL = svg.append("text")
+				.attr("text-anchor", "end")
+				.attr("x", width)
+				.attr("y", height+margin.top+margin.bottom)
+				.text("Time (years)");
 
+			// // Add Y axis label:
+			var Y_AXIS_LABEL = svg.append("text")
+				.attr("text-anchor", "end")
+				.attr("x", 0)
+				.attr("y", -20 )
+				.text("# of baby born")
+				.attr("text-anchor", "start")
+
+			// Wrapper for update function
+			function update_wrapper(data_, shift_val) {
+				// Update y axis
+				y_axis.domain([ 0, shift_val ]);
+				Y_AXIS.transition().duration(1000).call(d3.axisLeft(y_axis).ticks(10))
+				// Get selection of data
+				var chart_selection = inner.select("g") // If you don't select group first
+										 .selectAll("path") // one area/path is shifted down
+										 .data(data_)
+				// Catch every data element not yet been bound to the selected DOM elements
+				chart_selection
+					.enter()
+					.append("path")
+					.attr("class", function(d) { return "myArea " + d.key })
+					.merge(chart_selection)
+					.transition()
+					.duration(1000)
+					.style("fill", (d) => colorScale(d.key))
+					.attr("d", d3.area()
+						.x((d) => x_axis(d.data.year))
+						.y0((d) => y_axis(d[0]+shift_val))
+						.y1((d) => y_axis(d[1]+shift_val)));
+				// Remove the already existing DOM elements without an associated data item after enter()
+				chart_selection.exit() 
+					.remove();		
+			}
+
+			// Individual update functions for full and expanded data
+			function update_full() {
+				update_wrapper(stackedData, y_max);
+			}
+			function update_expanded() {
+				update_wrapper(stackedDataExpanded, 1);
+			}
+
+			// Get buttons to trigger chart update
+			var full_range_button = d3.select("#full-range")
+			var expanded_button = d3.select("#expanded")
+
+			// Implement click functionality
+			expanded_button.on("click",function(){update_expanded()})
+			full_range_button.on("click",function(){update_full()})
+
+			// Initialize chart with full range
+			update_full()
+
+
+			// What to do when one group is hovered
+			var highlight = function(d){
+				console.log(d)
+				// reduce opacity of all groups
+				d3.selectAll(".myArea")
+					.transition()
+					.duration(300)
+					.style("opacity", .1)
+				// expect the one that is hovered
+				d3.select("."+d)
+					.transition()
+					.duration(300)
+					.style("opacity", 1)
+			}
 			
-			// svg.append("path")
-			// 	.datum(stackedData)
-			// 	.attr("fill", (d) => colorScale(d.key))
-			// 	.attr("d", areaGen)
-				
+				// And when it is not hovered anymore
+			var noHighlight = function(d){
+				d3.selectAll(".myArea")
+				.transition()
+				.duration(500)
+				.style("opacity", 1)
+			}
 
-			// Show the areas
-			// svg.selectAll(".areas")
-			// 	.data(stackedData)
-			//     .enter()
-			// 	.append("path")	
-			// 		.style("fill", function(d) { return color(energy_types[d.key]); })
-			// 		.attr("d", d3.area()
-			// 		.x(function(d, i) { return x_axis(d.data.year); })
-			// 		.y0(function(d) { return y_axis(d[0]); })
-			// 		.y1(function(d) { return y_axis(d[1]); })
-			// 	)
+			// Trick to capitalize first letter of string (for legend)
+			const capitalizeFirstLetter = ([ first, ...rest ], locale = navigator.language) =>
+  				first.toLocaleUpperCase(locale) + rest.join('')
 
-			// Show the areas
-			// svg.append("path")
-			// 	.datum(stackedData)
-			// 	.style("fill", function(d) {return color(energy_types[d.key]); })
-			// 	.attr("d", d3.area()
-			// 		.x(function(d, i) { return x_axis(d.year); })
-			// 		.y0(function(d) { return y_axis(d[0]); })
-			// 		.y1(function(d) { return y_axis(d[1]); }))
+			// Rectangles for the legend
+			var size = 20
+			svg.selectAll("myrect")
+				.data(energy_keys.slice(1))
+				.enter()
+				.append("rect")
+				.attr("x", function(d,i){ return width/3 + i*(size+50)})
+				.attr("y", 0)
+				.attr("width", size)
+				.attr("height", size)
+				.style("fill", function(d){ return colorScale(d)})
+				.on("mouseover", highlight)
+				.on("mouseleave", noHighlight)
+		
+			// Labels associated to each rectangle in legend
+			svg.selectAll("mylabels")
+				.data(energy_keys.slice(1))
+				.enter()
+				.append("text")
+				.attr("x", function(d,i){ return width/3 + i*(size+50)})
+				.attr("y", size*1.5)
+				.style("fill", function(d){ return colorScale(d)})
+				.text(function(d){ return capitalizeFirstLetter(d)})
+				.attr("text-anchor", "left")
+				.style("alignment-baseline", "middle")
+				.on("mouseover", highlight)
+				.on("mouseleave", noHighlight)
+			
 		});
 	}
 }
